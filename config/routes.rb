dc9d21c7003eda1cb7e81954dcc2568d2479062e
Callum Tan <mullacyo@hotmail.com>
2019-03-24 11:55:40 +0800

    Added rewards.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'rewards/index'
   root 'welcome#index'
   
   resources :users
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,25 @@
 
 user = {}
 user['password'] = '123qwe'
-# user['password_confirmation'] = '123qwe'
 
 ActiveRecord::Base.transaction do
   20.times do 
     user['first_name'] = Faker::Name.first_name
     user['last_name'] = Faker::Name.last_name
     user['email'] = Faker::Internet.email
-    user['waste'] = rand(0..1000)
-
+    user['waste'] = rand(0.0..1000.0)
+    user['roles'] = rand(0..1)
     User.create(user)
+  end
+end 
+
+reward = {}
+
+ActiveRecord::Base.transaction do
+  20.times do 
+    reward['name'] = Faker::App.name
+    reward['sponsor'] = Faker::Name.name
+    reward['price'] = rand(5000..10000)
+    Reward.create(reward)
   end
 end 
